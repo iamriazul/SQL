b225e17435d9a8c88eedece35a75780311bbab08
iamriazul <119729025+iamriazul@users.noreply.github.com>
2026-07-01 02:57:20 +0600

    Add files via upload

---
+++ project/Pizzaburg/table_creation.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE pizzaburg ;
+use pizzaburg;
+
+create table orders (
+order_id int not null,
+order_date date not null,
+order_time time not null,
+primary key(order_id)
+);
+
+
+create table order_details (
+order_details_id int not null,
+order_id int not null,
+pizza_id text not null,
+quantity int not null,
+primary key(order_details_id )
+);
+
+-- Retrieve the total number of orders placed.
+select count(order_id) as total_orders from orders;
